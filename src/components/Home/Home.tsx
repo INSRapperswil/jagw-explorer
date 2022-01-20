c284980792e70a7f39fddb793d72b97663136f70
Michel Bongard <michel_bongard@hotmail.com>
2022-01-20 19:34:01 +0100

    set font-size globally to 62.5%

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,32 @@
 const Home = (): JSX.Element => {
-  return <div className="Home">Welcome to JAGW Explorer.</div>;
+  return (
+    <div className="TopologyPage Content">
+      <div className="PageTitle">JAGW Explorer</div>
+      <div className="TopologyPage-Intro">
+        <p>
+          The available topology data in the Jalapeño API Gateway does not
+          change dynamically. What topology data is available and how it is
+          structured can be gathered from the protobuf definitions either in the{' '}
+          <a
+            href="https://jalapeno-api-gateway.github.io/jagw-docs/docs/api/messages"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            official documentation
+          </a>{' '}
+          or directly from the{' '}
+          <a
+            href="https://github.com/jalapeno-api-gateway/protorepo"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            base repository
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
